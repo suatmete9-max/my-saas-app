cc1a7ee48eb12b06b5424ae4c99a0b14edbae30c
suatmete9-max <suatmete9@gmail.com>
2026-08-26 23:25:34 +0530

    fix: remove dollar sign from publishable key

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider publishableKey="pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXYk">
+    <ClerkProvider publishableKey="pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY">
       <html lang="en">
         <body>{children}</body>
       </html>
--- middleware.js
+++ middleware.js
@@ -1,9 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
-  publishableKey: 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXYk',
+  publishableKey: 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY',
   secretKey: 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1',
-  encryptionKey: 'my-super-secret-custom-key-12345',
 });
 
 export const config = {
