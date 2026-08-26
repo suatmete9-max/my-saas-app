a83708b15281f953e17f4aab14dac570e77f7691
suatmete9-max <suatmete9@gmail.com>
2026-08-26 23:17:19 +0530

    add clerk encryption key bypass

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware({
   publishableKey: 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXYk',
   secretKey: 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1',
+  encryptionKey: 'my-super-secret-custom-key-12345',
 });
 
 export const config = {
