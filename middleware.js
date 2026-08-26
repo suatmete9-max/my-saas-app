00aaefb942ce2363edd5018df6f5083db48f8569
suatmete9-max <suatmete9@gmail.com>
2026-08-26 23:35:07 +0530

    update layout code

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  publishableKey: 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY',
-  secretKey: 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1',
-});
+process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY = 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY';
+process.env.CLERK_SECRET_KEY = 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1';
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
