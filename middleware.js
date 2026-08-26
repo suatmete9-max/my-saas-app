fbc0318829e1721d0efa3b52a5e1fc7ad0833d07
suatmete9-max <suatmete9@gmail.com>
2026-08-26 23:04:05 +0530

    final direct fix for clerk middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
   publishableKey: 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXYk',
-  secretKey: process.env.CLERK_SECRET_KEY,
+  secretKey: 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1',
 });
 
 export const config = {
