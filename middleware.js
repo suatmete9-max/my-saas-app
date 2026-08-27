c60fe38120dfbcad4fed59fcd3ff703a5b6ee172
suatmete9-max <suatmete9@gmail.com>
2026-08-27 06:02:04 +0530

    temp: disable middleware for debugging

--- middleware.js
+++ middleware.js
@@ -1,8 +1,5 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY = 'pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY';
-process.env.CLERK_SECRET_KEY = 'sk_test_ZeIP2q9EK78Z4PeC2OhAmUH9IQMtqVyvHEJRUHcnl1';
-
 export default clerkMiddleware();
 
 export const config = {
