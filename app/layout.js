4d2cf9d6302fb6b30f0bb03dfc4cea4e009eda25
suatmete9-max <suatmete9@gmail.com>
2026-08-27 06:05:23 +0530

    temp: disable ClerkProvider in layout

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,9 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider publishableKey="pk_test_Y2hhbXBpb24tbW91c2UtOTA2NS5jbGVyay5hY2NvdW50cy5kZXY">
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   )
 }
